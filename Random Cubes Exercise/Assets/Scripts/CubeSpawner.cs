97812826a9e497cab12c852bf715059a254df8fb
William Pittenger <106987854+wbp1702@users.noreply.github.com>
2023-09-07 11:58:06 -0500

    Random Colored Cubes

--- Random Cubes Exercise/Assets/Scripts/CubeSpawner.cs
+++ Random Cubes Exercise/Assets/Scripts/CubeSpawner.cs
@@ -9,15 +9,13 @@ public class RandomCubesExercise : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //print("Hello World");
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        //print("Hello World");
-
-        GameObject cube = Instantiate(cubePrefab);
+        GameObject cube = Instantiate<GameObject>(cubePrefab);
         Material mat = cube.GetComponent<MeshRenderer>().material;
         mat.color = Random.ColorHSV(0, 1, 0.5f, 1, 0.75f, 1);
     }
